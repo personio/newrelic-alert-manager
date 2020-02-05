280f23edea7db903afd101f2235e573663ecbe11
Filip Petkovski <filip.petkovski@personio.de>
2020-02-05 12:06:14 +0100

    Change type names

--- pkg/apis/io/v1alpha1/doc.go
+++ pkg/apis/io/v1alpha1/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha1 contains API Schema definitions for the io v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=io.fpetkovski.newrelic
+// +groupName=newrelic.fpetkovski.io
 package v1alpha1
--- pkg/apis/io/v1alpha1/register.go
+++ pkg/apis/io/v1alpha1/register.go
@@ -2,7 +2,7 @@
 
 // Package v1alpha1 contains API Schema definitions for the io v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=io.fpetkovski.newrelic
+// +groupName=newrelic.fpetkovski.io
 package v1alpha1
 
 import (
@@ -12,7 +12,7 @@ import (
 
 var (
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "io.fpetkovski.newrelic", Version: "v1alpha1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: "newrelic.fpetkovski.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
